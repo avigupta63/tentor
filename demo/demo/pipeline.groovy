9ebf21355d795e5626b922f9bbc94e28b5e95b3c
avigupta63 <avi.gupta63@gmail.com>
2025-06-30 10:52:23 +0000

    pipeline

---
+++ demo/demo/pipeline.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/avigupta63/tentor.git'
+                // Add your pull commands here
+            }
+        }
+        stage('build') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+                // Add your test commands here
+            }
+        }
+        stage('test') {
+            steps {
+                echo 'test success...'
+                // Add your deployment commands here
+            }
+        }
+        stage('deploy') {
+            steps {
+                echo 'deploy success...'
+                // Add your deployment commands here
+            }
+        }
+    }
+
+
+}
