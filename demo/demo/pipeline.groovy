3f3ee98d0e5b709dbf034ff036736b3193771fd2
avigupta63 <avi.gupta63@gmail.com>
2025-07-01 03:50:08 +0000

    m

--- demo/demo/pipeline.groovy
+++ demo/demo/pipeline.groovy
@@ -24,11 +24,13 @@ pipeline {
 
                 stage('Deploy') {
     steps {
-        sh '''
-        scp -i ohio.pem demo/demo/target/*.jar ubuntu@18.191.129.243:/home/ubuntu/
-        ssh -i ohio.pem ubuntu@18.191.129.243 "pkill -f 'java -jar' || true && nohup java -jar /home/ubuntu/*.jar > app.log 2>&1 &"
-        '''
+          sshagent (credentials: ['ec2-ssh-ohio']) {
+                    sh '''
+                        scp -o StrictHostKeyChecking=no demo/demo/target/demo-0.0.1-SNAPSHOT.jar ubuntu@18.191.129.243:/home/ubuntu/
+                        ssh -o StrictHostKeyChecking=no ubuntu@18.191.129.243 'pkill -f "java -jar" || true && nohup java -jar /home/ubuntu/demo-0.0.1-SNAPSHOT.jar > app.log 2>&1 &'
+                    '''
     }
 }
+                }
     }
 }
